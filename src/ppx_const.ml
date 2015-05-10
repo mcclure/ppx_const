21be570ed3372af4290723558a6622e51d118313
Reynir Björnsson <reynir@reynir.dk>
2015-05-10 22:13:08 +0200

    Use ppx_metaquot

--- src/ppx_const.ml
+++ src/ppx_const.ml
@@ -23,28 +23,29 @@ let const_mapper argv =
           begin match process mapper exp with
             (* Currently only match with ifthenelse *)
             | { pexp_loc  = loc;
-                pexp_desc = Pexp_ifthenelse( {pexp_loc=cond_loc;pexp_desc=cond_desc},
-                  then_clause, else_option) } ->
+                pexp_desc = Pexp_ifthenelse (cond, then_clause, else_opt) } ->
               (* Used by = and <> *)
               let pairTest x y op = 
                 match x,y with
                 | Pexp_constant x, Pexp_constant y -> op x y
                 | _ ->
                   raise (Location.Error (
-                      Location.error ~loc:cond_loc "[%const if...] does not know how to compare these two expressions"))
+                      Location.error ~loc:cond.pexp_loc "[%const if...] does not know how to compare these two expressions"))
               in
               (* Evaluate conditional *)
-              let which = match cond_desc with
-                | Pexp_construct ({txt=Lident "true"},None) -> true
-                | Pexp_construct ({txt=Lident "false"},None) -> false
-                | Pexp_apply( {pexp_desc=Pexp_ident({txt=Lident "=" })}, [_,{pexp_desc=x};_,{pexp_desc=y}] ) -> pairTest x y (=)
-                | Pexp_apply( {pexp_desc=Pexp_ident({txt=Lident "<>"})}, [_,{pexp_desc=x};_,{pexp_desc=y}] ) -> pairTest x y (<>)
+              let which = match cond with
+                | [%expr true] -> true
+                | [%expr false] -> false
+                | [%expr [%e? x] = [%e? y]] ->
+                  pairTest x.pexp_desc y.pexp_desc (=)
+                | [%expr [%e? x] <> [%e? y]] ->
+                  pairTest x.pexp_desc y.pexp_desc (<>)
                 | _ ->
                   raise (Location.Error (
-                      Location.error ~loc:cond_loc "[%const if...] does not know how to interpret this kind of expression"))
+                      Location.error ~loc:cond.pexp_loc "[%const if...] does not know how to interpret this kind of expression"))
               in
               (* Depending on value of conditional, replace self extension node with either the then or else clause contents *)
-              if which then then_clause else (match else_option with Some x -> x | _ ->
+              if which then then_clause else (match else_opt with Some x -> x | _ ->
                 (* Or, if the else clause is selected but is not specified, a () *)
                 Ast_helper.with_default_loc loc (fun _ -> Ast_convenience.unit ()))
             (* Failed to match Pexp_ifthenelse, so fail *)
